92aa8c798b62e139c150a0e42e18b41087da6660
Oliver Tappin <olivertappin@gmail.com>
2016-03-05 18:47:47 +0000

    Added webhook controller

---
+++ app/Http/Controllers/WebhookController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class WebhookController extends Controller
+{
+    public function handleWebhook($payload)
+    {
+        $this->handleGithubPush();
+        $this->handleDeploybotDeployment();
+    }
+
+    /**
+     * Handle a GitHub webhook.
+     *
+     * @param  array  $payload
+     * @return Response
+     */
+    public function handleGithubPush($payload)
+    {
+        // Handle The Event
+    }
+
+    /**
+     * Handle a DeployBot webhook.
+     *
+     * @param  array  $payload
+     * @return Response
+     */
+    public function handleDeploybotDeployment($payload)
+    {
+        // Handle The Event
+    }
+}
